a0e80e4d78df8bddad0c1be51301ae3bc8476bd7
Adam McLean <adam.mclean1@gmail.com>
2015-02-21 08:24:14 -0800

    add show controller, show parameters and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,26 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   	
+  	def show
+  		@article = Article.find(params[:id])
+  	end
+  	
   	def new
   	end
 
   	#render method with key=>plain; value=>params[:article].inspect
   	#params method is an object
   	def create
-  		render plain: params[:article].inspect
+  		#We define article_params in 'private'
+  		@article = Article.new(article_params)
+  		
+  		@article.save
+  		redirect_to @article
   	end
+
+  	private
+  		def article_params
+  			params.require(:article).permit(:title, :text))
+			end
+
 end
