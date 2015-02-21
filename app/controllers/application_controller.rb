1e6d17a9f1e58281b0070b1210369c88387dfbce
Adam McLean <adam.mclean1@gmail.com>
2015-02-21 08:38:44 -0800

    use link_to helper to add controller links to views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   	
+  	def index
+  		@articles = Article.all
+  	end
+
   	def show
   		@article = Article.find(params[:id])
   	end
-  	
+
   	def new
   	end
 
@@ -22,7 +26,7 @@ class ApplicationController < ActionController::Base
 
   	private
   		def article_params
-  			params.require(:article).permit(:title, :text))
+  			params.require(:article).permit(:title, :text)
 			end
 
 end
