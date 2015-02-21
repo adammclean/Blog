f69eb373ba48dec9a75afa743b4618ee04962e51
Adam McLean <adam.mclean1@gmail.com>
2015-02-21 10:32:57 -0800

    add error form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,32 @@
 class ArticlesController < ApplicationController
+  def index
+  	@articles = Article.all
+  end
+
+  def show
+  	article = Article.find(params[:id])
+  end
+
+  def new
+  	@article = Article.new
+  end
+
+  #render method with key=>plain; value=>params[:article].inspect
+  #params method is an object
+  def create
+  	#We define article_params in 'private'
+  	@article = Article.new(article_params)
+  		
+  	@article.save
+  	redirect_to @article
+
+  	else
+  		render 'new'
+  	end
+  end
+  
+  private
+  	def article_params
+  		params.require(:article).permit(:title, :text)
+		end
 end
