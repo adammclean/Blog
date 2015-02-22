6f7443d4873eb5c6ca8f34af9bd316ee661eabf0
Adam McLean <adam.mclean1@gmail.com>
2015-02-22 12:01:55 -0800

    auth from scratch finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
-	attr_accessor :password
+	attr_accessible :email, :password, :password_confirmation
 
+	attr_accessor :password
+	before_save :encrypt_password
+	
 	validates_confirmation_of :password
 	validates_presence_of :password, :on => :create
 	validates_presence_of :email
