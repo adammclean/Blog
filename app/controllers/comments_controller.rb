a23a7f2ec662b167565185f91e1374fc55167c9f
Adam McLean <adam.mclean1@gmail.com>
2015-02-22 10:22:52 -0800

    complete CRUD functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,25 @@
 class CommentsController < ApplicationController
+	
+	http_basic_authenticate_with name: 'dhh' password: 'secret',
+only: :destroy
+
+
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
+		
+		####Why do we say (@article) at the end here?
 		redirect_to article_path(@article)
 	end
 
+	###We can't simply refer to the @article instance defined in Create?
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comment
+		@comment.destroy
+		redirect_to article_path(@article)
 	private
 		def comment_params
 			params.require(:comment).permit(:commenter, :body)
 		end
-
 end
