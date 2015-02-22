5c44e8dffa8e3be5a182dc41d9706ef061a7e85b
Adam McLean <adam.mclean1@gmail.com>
2015-02-22 11:34:40 -0800

    modify sessions controller and user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,14 @@ class SessionsController < ApplicationController
   def new
   end
 
+  #In railscast '#' is root_url; but we need to change that
   def create
-  	user = User.authenticate()
-end
+  	user = User.authenticate(params[:email], params[:password])
+  	if user
+  		session[:user_id] = user.id
+  		redirect_to '#', :notice => "Logged in!"
+  	else
+  		flash.now.alert = "Invalid email or password"
+  		render "new"
+
+  end
