750fd0c38a39b59b7d9972199454120ad7d6143a
Adam McLean <adam.mclean1@gmail.com>
2015-02-22 11:25:56 -0800

    add user and sessions controller and forms

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.0'
 #Use PG gem
 gem 'pg'
+#Bcrypt
+gem 'bcrypt-ruby', :require => 'bcrypt'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   
+  get 'log_in' => 'sessions#new', :as => 'log_in'
+
+  get 'sign_up' => 'users#new', :as => 'sign_up'
+  # root :to => 'users#new'
+  resources :users
+
   root 'welcome#index'
 
   resources :articles do 
     resources :comments
+
+  resources :sessions
+
     
   end
 
