f2478328f06cbb41375b4b311634a1ebd5703dee
Adam McLean <adam.mclean1@gmail.com>
2015-02-22 16:32:10 -0800

    fix comment bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,16 @@
 class CommentsController < ApplicationController
 	
-	http_basic_authenticate_with name: 'dhh' password: 'secret',
-only: :destroy
+# 	http_basic_authenticate_with name: '' password: '',
+# only: :destroy
 
 
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
 		
-		####Why do we say (@article) at the end here?
 		redirect_to article_path(@article)
 	end
+
 
 	###We can't simply refer to the @article instance defined in Create?
 	def destroy
@@ -18,6 +18,8 @@ only: :destroy
 		@comment = @article.comment
 		@comment.destroy
 		redirect_to article_path(@article)
+	end
+
 	private
 		def comment_params
 			params.require(:comment).permit(:commenter, :body)
